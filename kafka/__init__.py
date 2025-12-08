1434af2c7f9805b910c57b33b5c9ffb0faa70d94
bharathparimanan@gmail.com <bharathparimanan@gmail.com>
2025-12-08 17:18:24 +0000

    kafka initial setup

---
+++ kafka/__init__.py
@@ -0,0 +1,5 @@
+from kafka.producer import produce_message
+
+__all__ = [
+    'produce_message'
+]
--- kafka/producer.py
+++ kafka/producer.py
@@ -1,2 +1,13 @@
 #kafka concepts
 #zookeeper server
+import pandas as pd
+
+df = pd.read_csv("data/raw/StrmVid/Subscription Cohort Analysis Data Dictionary.csv")
+
+if df.empty():
+    print('none')
+else:
+    print(df.head(3))
+
+def produce_message():
+    print("kafka producer initiated")
